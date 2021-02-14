6ee80e8d5da85a0643cf47855bff9eacab973db4
Kitty Giraudel <hugo.giraudel@gmail.com>
2021-02-14 13:46:38 +0100

    Add library

---
+++ index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+module.exports = [
+  'a[href]:not([tabindex^="-"]):not([inert])',
+  'area[href]:not([tabindex^="-"]):not([inert])',
+  "input:not([disabled]):not([inert])",
+  "select:not([disabled]):not([inert])",
+  "textarea:not([disabled]):not([inert])",
+  "button:not([disabled]):not([inert])",
+  'iframe:not([tabindex^="-"]):not([inert])',
+  'audio:not([tabindex^="-"]):not([inert])',
+  'video:not([tabindex^="-"]):not([inert])',
+  '[contenteditable]:not([tabindex^="-"]):not([inert])',
+  '[tabindex]:not([tabindex^="-"]):not([inert])',
+];
