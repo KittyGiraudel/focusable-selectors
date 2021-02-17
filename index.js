dd3fd8508eff9d5664f081be1d41bca5bb14e31c
Kitty Giraudel <hugo.giraudel@gmail.com>
2021-02-17 11:50:21 +0100

    Check for <object>, <embed> and <summary>

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 module.exports = [
   'a[href]:not([tabindex^="-"]):not([inert])',
   'area[href]:not([tabindex^="-"]):not([inert])',
+  'object:not([inert])',
+  'embed:not([inert])',
+  'details:not([inert]) > summary:first-of-type:not([inert])',
   'input:not([disabled]):not([inert])',
   'select:not([disabled]):not([inert])',
   'textarea:not([disabled]):not([inert])',
